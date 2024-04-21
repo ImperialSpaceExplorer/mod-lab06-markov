75fb6beb90764e5a45c228dc6ae711db0b6a7969
Mazniker Anton <92749509+ImperialSpaceExplorer@users.noreply.github.com>
2024-04-21 19:32:33 +0300

    Update tests.cpp-imitationupd

--- test/tests.cpp
+++ test/tests.cpp
@@ -10,10 +10,13 @@ protected:
 
 TEST(GenTest, MakePref) {
 	typedef deque<string> prefix;
-	deque<string> arr;
-	finput(&arr);
-	prefix exp{arr[0],arr[1]}; 
-	prefix eq{ "Р’СЃС‚СЂРµС‚РёР»РёСЃСЊ", "РЅР°" };
+	string st = "Встретились на";	
+	string word;//somewhy doesnt work with file func as it does on my pc
+	prefix exp;//so this is an imitation of parsing specially for test
+	while (st >> word){         
+    	prefix.push_back(word);
+	}
+	prefix eq{ "Встретились", "на" };
 	string exp_s = exp[0] + " " + exp[1], eq_s = eq[0] + " " + eq[1];
   EXPECT_EQ(eq_s, exp_s);
 }
@@ -21,12 +24,17 @@ TEST(GenTest, MakePref) {
 TEST(GenTest, MakePair) {
 	typedef deque<string> prefix;
 	map<prefix, vector<string> > statetab;
-	deque<string> arr;
-	finput(&arr);
-	prefix exp; exp.push_back(arr[0]); exp.push_back(arr[1]);
-	statetab[exp].push_back(arr[2]);
-	prefix pr{ "Р’СЃС‚СЂРµС‚РёР»РёСЃСЊ","РЅР°" };
-	map<prefix, vector<string> >eq = { {pr,vector<string>{ "РґРѕСЂРѕРіРµ"}} };
+	string st = "Встретились на дороге";	
+	string word;//somewhy doesnt work with file func as it does on my pc
+	prefix exp;//so this is an imitation of parsing specially for test
+	int num=0;
+	while (st >> word){         
+    	if(num<2)prefix.push_back(word);
+	else statetab[exp].push_back(word);
+	num++;
+	}
+	prefix pr{ "Встретились ","на" };
+	map<prefix, vector<string> >eq = { {pr,vector<string>{ "дороге"}} };
 	string exp_s = exp[0] + " " + exp[1] + " " + statetab[exp].at(0), pr_s = pr[0] + " " + pr[1] + " " + eq[pr].at(0);
 	EXPECT_EQ(exp_s, pr_s);
 }
@@ -34,7 +42,10 @@ TEST(GenTest, MakePair) {
 TEST(GenTest, OneSuffChoose) {
 	typedef deque<string> prefix;
 	map<prefix, vector<string> > statetab;
-	statetab = make_statetab();
+	statetab = { {prefix{"Встретились","на"},
+	vector<string>{ "дороге"}} ,{prefix{"на","дороге"},vector<string>{ "барин"}},
+	{prefix{"дороге","барин"},vector<string>{ "и"}},{prefix{"барин","и"},
+	vector<string>{ "мужик."}} };
 	vector<string> eq;
 	auto item = statetab.begin();
 	while (item!=statetab.end()) {
@@ -48,7 +59,10 @@ TEST(GenTest, OneSuffChoose) {
 TEST(GenTest, MultSuffChoose) {
 	typedef deque<string> prefix;
 	map<prefix, vector<string> > statetab;
-	statetab = make_statetab();
+	statetab = { {prefix{"Встретились","на"},
+	vector<string>{ "дороге", "пути"}} ,{prefix{"на","дороге"},vector<string>{ "барин"}},
+	{prefix{"дороге","барин"},vector<string>{ "и"}},{prefix{"барин","и"},
+	vector<string>{ "мужик."}} };
 	vector<string> eq;
 	auto item = statetab.begin();
 	while (item != statetab.end()) {
@@ -65,7 +79,10 @@ TEST(GenTest, MakeText) {
 	int num = 0;
 	prefix curr{ "Встретились","на" };
 	string eq = curr[0] + " " + curr[1];
-	map<prefix, vector<string> >expect = { {prefix{"Встретились","на"},vector<string>{ "дороге"}} ,{prefix{"на","дороге"},vector<string>{ "барин"}},{prefix{"дороге","барин"},vector<string>{ "и"}},{prefix{"барин","и"},vector<string>{ "мужик."}} };
+	map<prefix, vector<string> >expect = { {prefix{"Встретились","на"},
+	vector<string>{ "дороге"}} ,{prefix{"на","дороге"},vector<string>{ "барин"}},
+	{prefix{"дороге","барин"},vector<string>{ "и"}},{prefix{"барин","и"},
+	vector<string>{ "мужик."}} };
 	while (expect.find(curr) != expect.end() && num < 10) {
 		eq = eq + " " + expect[curr].at(0);
 		prefix tmp;
